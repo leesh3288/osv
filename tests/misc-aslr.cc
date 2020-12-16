32d5dfa6e294a8f550aa2cf5e2ad6939f7984227
KimDongok <ehddhr4454@kaist.ac.kr>
2020-12-15 23:09:48 -0800

    add basic aslr test case

---
+++ tests/misc-aslr.cc
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <sys/mman.h>
+#include <stdlib.h>
+#include <thread>
+#include <osv/sched.hh>
+
+int main(int argc, char *argv[])
+{
+    int stack_var;
+    void * small_heap_addr, * large_heap_addr, * mmap_addr;
+
+    small_heap_addr = malloc(0x80);
+    large_heap_addr = malloc(0x10000);
+    mmap_addr = mmap(nullptr, 0x1000, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
+
+    
+
+    printf("mmaped address : %p\n", mmap_addr);
+    printf("main program stack address : %p\n", &stack_var);
+
+    std::thread t = std::thread([]() {
+        int thread_stack_var;
+        printf("thread stack address : %p\n", &thread_stack_var);
+    });
+
+    t.join();
+
+    printf("small heap address : %p\n", small_heap_addr);
+    printf("large heap address : %p\n", large_heap_addr);
+
+    printf("main program address (main function) : %p\n", main);
+    printf("kernel base address (sched::cpu::schedule function) : %p\n", sched::cpu::schedule);
+    
+    
+    return 0;
+}
