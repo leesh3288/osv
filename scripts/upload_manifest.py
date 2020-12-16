81c2f92ee45146c950e88e8288442903684a1e7f
KimDongok <ehddhr4454@kaist.ac.kr>
2020-12-16 03:18:23 -0800

    fix upload_manifest.py (force to using random)

--- scripts/upload_manifest.py
+++ scripts/upload_manifest.py
@@ -155,7 +155,7 @@ def main():
         kernel_mode_flag = '-k --kernel-path build/release/loader-stripped.elf'
     else:
         kernel_mode_flag = ''
-    osv = subprocess.Popen('cd ../..; scripts/run.py %s --vnc none -m 512 -c1 -i "%s" --block-device-cache unsafe -s -e "--norandom --nomount --noinit /tools/mkfs.so; /tools/cpiod.so --prefix /zfs/zfs/; /zfs.so set compression=off osv" --forward tcp:127.0.0.1:%s-:10000' % (kernel_mode_flag,image_path,upload_port), shell=True, stdout=subprocess.PIPE)
+    osv = subprocess.Popen('cd ../..; scripts/run.py %s --vnc none -m 512 -c1 -i "%s" --block-device-cache unsafe -s -e "--nomount --noinit /tools/mkfs.so; /tools/cpiod.so --prefix /zfs/zfs/; /zfs.so set compression=off osv" --forward tcp:127.0.0.1:%s-:10000' % (kernel_mode_flag,image_path,upload_port), shell=True, stdout=subprocess.PIPE)
 
     upload(osv, manifest, depends, upload_port)
 
